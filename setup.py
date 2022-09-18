2f80d1c79cf56253109c8dad1b432fe821d3297a
Todysheep <97968466+Todysheep@users.noreply.github.com>
2022-09-18 22:43:17 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nonebot_plugin_bottle",
-    version="0.1",
+    version="0.1.1",
     author="Todysheep",
     author_email="todysheep@163.com",
     description="Bottle post plugin in Nonebot",
@@ -13,11 +13,14 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Todysheep/nonebot_plugin_bottle",
     packages=setuptools.find_packages(),
-    install_requires=['Pillow>=5.1.0', 'numpy==1.14.4'],
+    install_requires=[
+        'nonebot2>=2.0.0-beta.2',
+        'nonebot-adapter-onebot>=2.0.0b1',
+        'nonebot-adapter-onebot',],
     entry_points={},
+    license='GNU GPLv3',
     classifiers=(
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
 )
